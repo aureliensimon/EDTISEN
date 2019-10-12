4139959b5ccd573132ed45d248f994424a6349f5
aureliensimon <aureliensimon29260@gmail.com>
2019-10-12 20:46:35 +0200

    Add dynamic menu to edt

--- js/edt.js
+++ js/edt.js
@@ -9,3 +9,17 @@ function getTime () {
 
     document.getElementById('date').innerHTML = date;
 }
+
+function changeCSS (div) {
+    let alldiv = Array.from(div.parentNode.children);
+
+    alldiv.forEach(element => {
+        if(element.id != div.id){
+            document.getElementById(element.id).style.color = '#8f919c';
+            document.getElementById(element.id).style.borderBottom = '0px';
+        }        
+    });
+    
+    div.style.color = 'white';
+    div.style.borderBottom = '3px solid #0478EB';
+}
